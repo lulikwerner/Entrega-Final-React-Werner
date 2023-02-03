b87a78afcfb52f1cb33a0002592daebb194f775c
Lucila Werner <lulikwerner@gmail.com>
2023-02-02 22:02:30 -0500

    correccion boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 import NavBar from './componentes/NavBar/NavBar'
 import 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,38 +1,48 @@
 import CartWidget from "../CartWidget/CartWidget"
 import'../NavBar/NavBar.css'
 import 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function NavBar(){
     return(
-
-        
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top App">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a className="navbar-brand" href="#">Mia Jewelers</a>
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0 list" >
-        
-          <li className="nav-item">
-            <a className="nav-link" href="#">Relojes</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Joyas</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Servicios</a>
-          </li>
-        </ul>
-     <section className='posit '>
+<nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top App">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Mia Jewelers</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Servicios</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Relojes</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Joyas
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Pulseras</a></li>
+            <li><a className="dropdown-item" href="#">Anillos</a></li>
+            <li><a className="dropdown-item" href="#">Collares</a></li>
+          </ul>
+        </li>
+      </ul>
+     
+      <section className='posit d-flex'>
       <CartWidget/>
       <ion-icon name="bag-outline"></ion-icon>
       </section>
       <a className="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3 posita" href="#">LogIn</a>
       </div>
-    </nav>
-  
+    
+   
+  </div>
+</nav>
+        
+
 
 
  
